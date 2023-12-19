d585a94ce3abbe4af2f01d356daa731bbca24b78
NgHuyZzZ <145020294+NgHuyZzZ@users.noreply.github.com>
2023-12-19 11:30:12 +0700

    Update NgHuyHub.lua

--- scripts/NgHuyHub.lua
+++ scripts/NgHuyHub.lua
@@ -4,11 +4,11 @@ toclipboard("https://discord.gg/b4Myfv76UU")
 repeat task.wait() until game:IsLoaded()
 local TablePlace = {13772394625,14915220621,14732610803,15264892126}
 if table.find(TablePlace,game.PlaceId) then 
-    loadstring(game:HttpGet("https://nghuyy.ddns.net/scripts/BladeBall.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NgHuyZzZ/nghuyzzz.github.io/main/scripts/BladeBall.lua"))()
 elseif game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
-    loadstring(game:HttpGet("https://nghuyy.ddns.net/scripts/BloxFruits.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NgHuyZzZ/nghuyzzz.github.io/main/scripts/BloxFruits.lua"))()
 elseif game.PlaceId == 3101667897 then
-    loadstring(game:HttpGet('https://shz.al/~nghuylos"))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/NgHuyZzZ/nghuyzzz.github.io/main/scripts/LOS.lua"))()
 else
     game.Players.LocalPlayer:Kick("Not Support")
 end
